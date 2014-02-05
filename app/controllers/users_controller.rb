1e787537e6e9f2e9291c7b713c99070415c8001a
Bruno Oliveira <bruno@abstractj.org>
2014-02-05 01:11:09 -0200

    Example of integration with Krypt and Java application server

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,94 @@
+require 'krypt'
+
+class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+
+  # GET /users
+  # GET /users.json
+  def index
+    @users = User.all
+  end
+
+  # GET /users/1
+  # GET /users/1.json
+  def show
+  end
+
+  # GET /users/new
+  def new
+    @user = User.new
+  end
+
+  # GET /users/1/edit
+  def edit
+  end
+
+  # POST /users
+  # POST /users.json
+  def create
+    # Simple example of usage for digests
+    digest = Krypt::Digest::SHA256.new
+
+    puts "Digest: #{digest.hexdigest(user_params)}"
+
+    # This is just a simple example of usage for PBKDF2 functions
+    # it does not represent any best practices of how to properly store passwords
+    password = "strong"
+
+    pbkdf2 = Krypt::PBKDF2.new(digest)
+    salt = OpenSSL::Random.random_bytes(16)
+    len = digest.digest_length
+    iter = 20000
+
+    hash = pbkdf2.generate_hex(user_params[:password], salt, iter, len)
+    puts "PBKDF2: #{hash}"
+
+    @user = User.new(user_params)
+    @user.password = hash
+
+    respond_to do |format|
+      if @user.save
+        format.html { redirect_to @user, notice: 'User was successfully created.' }
+        format.json { render action: 'show', status: :created, location: @user }
+      else
+        format.html { render action: 'new' }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /users/1
+  # PATCH/PUT /users/1.json
+  def update
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to @user, notice: 'User was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: 'edit' }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /users/1
+  # DELETE /users/1.json
+  def destroy
+    @user.destroy
+    respond_to do |format|
+      format.html { redirect_to users_url }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def user_params
+      params.require(:user).permit(:email, :password)
+    end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TorqueboxKryptExample::Application.config.secret_key_base = '59be3718dc99512100d06162d7810dfe91245e7723da82454d545ce8a62ef7e1c802d99ff40e447fa81a3d052ccf3250c6180b6a6f1edaeb9398b9254aa816f4'
